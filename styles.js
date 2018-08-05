4549d093271c39ee3ca80514e4a3d88ecbea045d
dg-05 <42015618+dg-05@users.noreply.github.com>
2018-08-04 18:57:44 -0600

    Add files via upload

--- styles.js
+++ styles.js
@@ -1,16 +1,3 @@
-function changeBurger(){
-	let burgerBar = getElementByClassName("bar");
-	let burgerBarAfter = getElementByClassName("bar:before");
-	let burgerBarBefore = getElementByClassName("bar:after");
-	if (burgerBar.style.transform === "none") {
-
-        burgerBar.style.transform = "rotate(47deg)";
-        
-        burgerBarAfter.style.transform = "rotate(81deg)";
-		burgerBarAfter.style.top = "-10%";        
-        
-        burgerBarBefore.style.width = "0";
-    } else {
-        burgerBar.style.display = "none";
-    }
+function changeBurger(x){
+	x.classList.toggle("change");
 }
